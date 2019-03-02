00ed00e430f33eebd39cd4a0ad29c8bc9ea0164c
Kieran Eglin <kieran.eglin@gmail.com>
2019-03-02 15:54:09 -0800

    Added test for checking README version

--- test/encrypted_secrets_test.exs
+++ test/encrypted_secrets_test.exs
@@ -1,4 +1,22 @@
 defmodule EncryptedSecretsTest do
   use ExUnit.Case
   doctest EncryptedSecrets
+
+  test "README install version check" do
+    app = :encrypted_secrets
+
+    app_version = "#{Application.spec(app, :vsn)}"
+    readme = File.read!("README.md")
+    [_, readme_versions] = Regex.run(~r/{:#{app}, "(.+)"}/, readme)
+
+    assert Version.match?(
+             app_version,
+             readme_versions
+           ),
+           """
+           Install version constraint in README.md does not match to current app version.
+           Current App Version: #{app_version}
+           Readme Install Versions: #{readme_versions}
+           """
+  end
 end
