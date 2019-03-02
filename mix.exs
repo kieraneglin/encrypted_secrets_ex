3a886450c9a74274b132c709dfb6be6165d977b8
Kieran Eglin <kieran.eglin@gmail.com>
2019-03-02 15:55:53 -0800

    Removed unneeded comments

--- mix.exs
+++ mix.exs
@@ -11,21 +11,17 @@ defmodule EncryptedSecrets.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, "~> 0.12"},
       {:yaml_elixir, "~> 2.1.0"},
       {:ex_crypto, "~> 0.10.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
