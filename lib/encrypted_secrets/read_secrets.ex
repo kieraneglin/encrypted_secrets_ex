5cff2006a0eea578ffcfe71b5c9c632a26d1ccb4
Kieran Eglin <kieran.eglin@gmail.com>
2019-03-01 19:55:20 -0800

    Added a read_into_file function; renamed read_file function

--- lib/encrypted_secrets/read_secrets.ex
+++ lib/encrypted_secrets/read_secrets.ex
@@ -1,8 +1,14 @@
 defmodule EncryptedSecrets.ReadSecrets do
-  def read_file(key, input_path) do
+  def read_into_map(key, input_path) do
     read_encrypted_file(input_path)
     |> unencrypt_file_contents(key)
     |> parse_yaml()
+  end
+
+  def read_into_file(key, input_path) do
+    read_encrypted_file(input_path)
+    |> unencrypt_file_contents(key)
+    |> write_temp_file(input_path)
   end
 
   defp read_encrypted_file(input_path) do
@@ -25,5 +31,18 @@ defmodule EncryptedSecrets.ReadSecrets do
 
   defp parse_yaml(yaml_string) do
     YamlElixir.read_from_string(yaml_string)
+  end
+
+  defp write_temp_file(yaml_string, input_path) do
+    working_directory = Path.dirname(input_path)
+    # Honestly, I'm not sure why Rails appends a random string to the tmp files.
+    # Maybe to avoid file collisions in a really dumb way?  Who knows.  Anyway, I'm doing it
+    random_file_suffix = :crypto.strong_rand_bytes(8) |> Base.encode16()
+    filename = "#{working_directory}/secrets_tmp_#{random_file_suffix}.yml"
+
+    case File.write(filename, yaml_string) do
+      :ok -> {:ok, filename}
+      {:error, err} -> throw("Error creating tempfile (#{err})")
+    end
   end
 end
