2b3c0d99a45bb66fa287231bfb94f7104fb3ad5d
Kieran Eglin <kieran.eglin@gmail.com>
2019-03-02 16:09:03 -0800

    Added license; prepared mix file for publishing

--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule EncryptedSecrets.MixProject do
       app: :encrypted_secrets,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/kieraneglin/encrypted_secrets_ex",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -22,6 +25,18 @@ defmodule EncryptedSecrets.MixProject do
       {:ex_doc, "~> 0.12"},
       {:yaml_elixir, "~> 2.1.0"},
       {:ex_crypto, "~> 0.10.0"}
+    ]
+  end
+
+  defp description() do
+    "A package that allows you to store encrypted application secrets in your VCS"
+  end
+
+  defp package() do
+    [
+      name: "EncryptedSecrets",
+      licenses: ["MIT"],
+      links: %{repo: "https://github.com/kieraneglin/encrypted_secrets_ex"}
     ]
   end
 end
